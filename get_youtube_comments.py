1069f2a6790c1e5b1fe44f73f7c184dc202399e6
Doyoon Lee <dynleedev@gmail.com>
2022-01-05 14:34:37 +0900

    Get more than 100 comments

--- get_youtube_comments.py
+++ get_youtube_comments.py
@@ -2,26 +2,30 @@ import pandas
 from googleapiclient.discovery import build
 import config
 
-video_id = '0e3GPea1Tyg'
+video_id = 'LgmbgAIebw0'
 
 comments = list()
 api_obj = build('youtube', 'v3', developerKey=config.api_key)
-response = api_obj.commentThreads().list(part='snippet,replies', videoId=video_id, maxResults=20).execute()
+response = api_obj.commentThreads().list(part='snippet,replies', videoId=video_id, maxResults=100).execute()
 
-for item in response['items']:
-    comment = item['snippet']['topLevelComment']['snippet']['textDisplay']
-    comments.append(comment)
+while response:
+    for item in response['items']:
+        comment = item['snippet']['topLevelComment']['snippet']['textDisplay']
+        comments.append(comment)
 
-        # if item['snippet']['totalReplyCount'] > 0:
-        #     for reply_item in item['replies']['comments']:
-        #         reply = reply_item['snippet']['textDisplay']
-        #         comments.append(reply)
+        if item['snippet']['totalReplyCount'] > 0:
+            for reply_item in item['replies']['comments']:
+                reply = reply_item['snippet']['textDisplay']
+                comments.append(reply)
 
-    # if 'nextPageToken' in response:
-    #     response = api_obj.commentThreads().list(part='snippet,replies', videoId=video_id, pageToken=response['nextPageToken'], maxResults=10).execute()
-    # else:
-    #     break
+    if len(comments)>500:
+        break
+
+    if 'nextPageToken' in response:
+        response = api_obj.commentThreads().list(part='snippet,replies', videoId=video_id, pageToken=response['nextPageToken'], maxResults=100).execute()
+    else:
+        break
 
 df = pandas.DataFrame(comments)
 path = './data/'
-df.to_csv(path+'comments.txt', index=None)
+df.to_csv(path+'comments.txt', index=None, columns=None)
